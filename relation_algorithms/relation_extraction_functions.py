5f3fbbe80399e4bf293d90da01a2e42126b4fe13
Zane Hutchens <zhut654@gmail.com>
2024-02-26 18:09:56 -0500

    New files and changed relation extractor to a class and debugged its issues

--- relation_algorithms/relation_extraction_functions.py
+++ relation_algorithms/relation_extraction_functions.py
@@ -15,76 +15,86 @@ llm = OpenAI()
 
 # For internal usage only
 def create_concept_graph_structure(param_list: list) -> dict:
-
     return_dict = {}
+
     for list_itm in param_list:
         return_dict[list_itm] = []
 
     return return_dict
 
 
-def identify_main_topics(textbook_link: str) -> list:
-    main_topics = llm(f"Please identify ten main topics from this textbook: {textbook_link}")
-    return [topic for topic in main_topics.split('\n')]
+class LLM_Relation_Extractor:
+
+    def __init__(self, textbook_link: str):
+        self.link = textbook_link
 
 
-def identify_learning_concepts_outcomes(chapter_dict: dict, link: str) -> list:
-
-    return_list = []
-    current_concept = ''
-    for chapter, chapter_name in chapter_dict.items():
-        current_concept = llm(f"Please identify the main learning outcomes and main learning concepts given {chapter}, the chapter name is {chapter_name}. Here is the textbook in which to retrieve them: {link}")
-        current_concept = re.sub(re.compile('[^a-zA-Z\s\.,!?]'), '', current_concept)
-        return_list.append(current_concept)
-    
-    return return_list
+    def identify_chapters(self):
+        # NOTE: This is very, very, inconsistent. Do not recommend using this.
+        chapters = llm(f"Please identify the chapters in this textbook: {self.link}")
+        return [chapter for chapter in chapters.split('\n')][2:]
 
 
-def association_identification(concept_list: list) -> dict:
-
-    concept_graph = create_concept_graph_structure(concept_list)
-    new_association = ''
-    for i in range(len(concept_list)):
-        for j in range(i + 1, len(concept_graph)):
-            new_association = llm(f"Please identify if there is an association between this concept: {concept_list[i]}, and this other concept: {concept_list[j]}. If there is NO association, please start your response with 'No' and 'No' only.")
-            new_association = re.sub(re.compile('[^a-zA-Z\s\.,!?]', '', new_association))
-            if new_association.split(',')[0].strip() != 'No':
-                concept_graph[concept_list[i]].append(concept_list[j])
-    
-    return concept_graph
+    def identify_main_topics(self) -> list:
+        main_topics = llm(f"Please identify ten main topics from this textbook: {self.link}")
+        return [topic for topic in main_topics.split('\n')][2:]
 
 
-def dependency_relation_extraction(keys: list, relations_dict = None | dict) -> dict:
-    
-    relation = ''
+    def identify_learning_concepts_outcomes(self, chapter_dict: dict) -> list:
+        return_list = []
+        current_concept = ''
 
-    if relations_dict == None:
-        relations_dict = create_concept_graph_structure(keys)
-
-    for i in range(len(keys)):
-        for j in range(len(keys)):
-            if i != j:
-                relation = llm(f"Is there a dependency relationship from {keys[i]} to {keys[j]}? If there is NOT, please respond with 'No' and 'No' only.")
-                relation = re.sub(re.compile('[^a-zA-Z\s\.,!?]',), '', relation)
-                if relation.split()[0] != 'No':
-                    relations_dict[keys[j]].append(keys[i])
-
-    return relations_dict
+        for chapter, chapter_name in chapter_dict.items():
+            current_concept = llm(f"Please identify the main learning outcomes and main learning concepts given {chapter}, the chapter name is {chapter_name}. Here is the textbook in which to retrieve them: {self.link}")
+            current_concept = re.sub(re.compile('[^a-zA-Z\s\.,!?]'), '', current_concept)
+            return_list.append(current_concept)
+        
+        return return_list
 
 
-def print_flat_graph(learning_concept_graph: dict) -> None:
+    def association_identification(self, concept_list: list) -> dict:
+        concept_graph = create_concept_graph_structure(concept_list)
+        new_association = ''
 
-    graph = graphviz.Graph()
+        for i in range(len(concept_list)):
+            for j in range(i + 1, len(concept_list)):
+                new_association = llm(f"Please identify if there is an association between this concept: {concept_list[i]}, and this other concept: {concept_list[j]}. If there is NO association, please start your response with 'No' and 'No' only.")
+                new_association = re.sub(re.compile('[^a-zA-Z\s\.,!?]'), '', new_association)
+                if new_association.split(',')[0].strip() != 'No':
+                    concept_graph[concept_list[i]].append(concept_list[j])
+        
+        return concept_graph
 
-    for key, values in learning_concept_graph.items():
-        graph.node(name = key)
-        for value in values:
-            graph.edge(key, value)
 
-    display(Image(graph.pipe(format = "png", renderer = "cairo")))
+    def dependency_relation_extraction(self, keys: list, relations_dict = None) -> dict:
+        relation = ''
+
+        if relations_dict == None:
+            relations_dict = create_concept_graph_structure(keys)
+
+        for i in range(len(keys)):
+            for j in range(len(keys)):
+                if i != j:
+                    relation = llm(f"Is there a dependency relationship from {keys[i]} to {keys[j]}? If there is NOT, please respond with 'No' and 'No' only.")
+                    relation = re.sub(re.compile('[^a-zA-Z\s\.,!?]'), '', relation)
+                    if relation.split()[0] != 'No':
+                        relations_dict[keys[j]].append(keys[i])
+
+        return relations_dict
 
 
-def print_interactive_graph() -> None:
-    # TODO: Finish code here to print an interactive graph 
-    print('hello!!')
+    def print_flat_graph(self, learning_concept_graph: dict) -> None:
+        graph = graphviz.Graph()
+
+        for key, values in learning_concept_graph.items():
+            graph.node(name = key)
+            for value in values:
+                graph.edge(key, value)
+
+        display(Image(graph.pipe(format = "png", renderer = "cairo")))
+
+
+    def print_interactive_graph(self) -> None:
+        # TODO: Finish code here to print an interactive graph 
+        print('hello!!')
 
